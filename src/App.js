81e8f542f993da80b52fddcd64a05779effcd217
Avichez <starkavichi@gmail.com>
2022-06-14 20:59:20 +0300

    feat: add fetch for pizzas data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import './scss/app.scss';
-import Header from './components/Header';
-import Categories from './components/Catgories';
-import Sort from './components/Sort';
-import PizzaItem from './components/PizzaItem';
-import pizzas from './assets/pizzas.json';
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Catgories";
+import Sort from "./components/Sort";
+import PizzaItem from "./components/PizzaItem";
+// import pizzas from "./assets/pizzas.json";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  // https://62a8c6edec36bf40bdadcca5.mockapi.io/items
+  useEffect(() => {
+    fetch("https://62a8c6edec36bf40bdadcca5.mockapi.io/items")
+      .then((response) => response.json())
+      .then((data) => {
+        data && setItems(data);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((item) => (
+            {items.map((item) => (
               <PizzaItem key={item.id} {...item} />
             ))}
           </div>
@@ -25,6 +37,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Catgories.jsx
+++ src/components/Catgories.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
 const Categories = (props) => {
-  const [activeCategory, setActiveCategory] = React.useState(0);
+  const [activeCategory, setActiveCategory] = React.useState(1);
 
-  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+  // const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+  const categories = [
+    { id: 1, name: 'Все' },
+    { id: 2, name: 'Мясные' },
+    { id: 3, name: 'Вегетарианская' },
+    { id: 4, name: 'Гриль' },
+    { id: 5, name: 'Острые' },
+    { id: 6, name: 'Закрытые' },
+  ]
 
   const chooseCategory = (id) => {
     setActiveCategory(id);
@@ -13,8 +21,8 @@ const Categories = (props) => {
     <div className="categories">
       <ul>
         {
-          categories.map((pizza, index) => (
-            <li key={index} onClick={() => chooseCategory(index)} className={activeCategory === index ? 'active' : ''}>{pizza}</li>
+          categories.map((pizza) => (
+            <li key={pizza.id} onClick={() => chooseCategory(pizza.id)} className={activeCategory === pizza.id ? 'active' : ''}>{pizza.name}</li>
           ))
         }
       </ul>
